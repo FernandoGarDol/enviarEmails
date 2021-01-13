bccf2df593f6940356dcd66fa2c01d471dec8c93
unknown <erick_2199quintana@hotmail.com>
2021-01-13 17:17:14 -0600

    first commit

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import  {paginaAdmin,paginaAdmin2, paginaInicio, paginaLoginGet, paginaLoginPost, paginaLogout, paginaReservacionesAdmin, paginaRestablecer, paginaTestimonialesAdmin, paginaViajesAdmin} from '../controllers/paginaControllers.js'
-import  {paginaNosotros} from '../controllers/paginaControllers.js'
+import { paginaReservaciones, Reservacion, cancelarReservaciones } from '../controllers/paginaControllers.js'
+import { paginaNosotros } from '../controllers/paginaControllers.js'
 import  {paginaViajes} from '../controllers/paginaControllers.js'
 import  {paginaTestimoniales} from '../controllers/paginaControllers.js'
-import  {paginaDetalleVIajes} from '../controllers/paginaControllers.js'
+import { paginaDetalleVIajes } from '../controllers/paginaControllers.js'
+import { guardarReservaciones } from '../controllers/reservacionesControllers.js'
 import  {eliminarTestimonial, guardarTestimoniales} from '../controllers/testimonialesController.js'
-import {eliminarViaje, guardarViaje} from '../controllers/viajesControllers.js'
+import { eliminarViaje, guardarViaje } from '../controllers/viajesControllers.js'
+import { cancelar } from '../controllers/cancelarReservaciones.js'
 import {paginaEmail} from '../controllers/Email.js'
 //import {paginaAdmin} from '../controllers/paginaControllers.js'
 
@@ -24,7 +27,12 @@ router.post('/login', paginaLoginPost);
 router.get('/admin',paginaAdmin);
 router.get('/admin2',paginaAdmin2);
 router.get('/testimonialesAdmin', paginaTestimonialesAdmin);
-router.get('/reservacionesAdmin',paginaReservacionesAdmin);
+router.get('/reservacionesAdmin', paginaReservacionesAdmin);
+router.get('/reservaciones', paginaReservaciones);
+router.get('/reservacion', Reservacion);
+router.post('/reservacion', guardarReservaciones);
+router.get('/cancelar', cancelarReservaciones);
+router.post('/cancelar', cancelar);
 router.get('/viajesAdmin', paginaViajesAdmin);
 router.get('/deleteTestimonial/:id', eliminarTestimonial);
 router.get('/deleteViaje/:id', eliminarViaje);
